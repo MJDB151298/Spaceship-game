e62e35df1cde29c21c19aca75709af7cc5f6a038
marco <20160370@ce.pucmm.edu.do>
2021-03-14 16:53:02 -0400

    Laser Behaviour

---
+++ Assets/Scripts/SpaceshipBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipBehaviourScript : MonoBehaviour
+{
+    public GameObject soapLaser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
