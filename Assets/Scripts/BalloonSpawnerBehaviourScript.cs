120ed8968a027e4cae812c56bd034e130d053fe5
marco <20160370@ce.pucmm.edu.do>
2021-03-15 21:27:18 -0400

    Balloon Spawner

---
+++ Assets/Scripts/BalloonSpawnerBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawnerBehaviourScript : MonoBehaviour
+{
+    public GameObject balloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnBalloon", 0f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnBalloon(){
+        Vector3 spawner = gameObject.transform.GetChild(Random.Range(0,5)).transform.position;
+        //Vector3 spawnerPosition = spawner.transform.position;
+        Instantiate(balloon, spawner, Quaternion.identity);
+    }
+}
--- Assets/Scripts/LaserBehaviourScript.cs
+++ Assets/Scripts/LaserBehaviourScript.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class LaserBehaviourScript : MonoBehaviour
 {
-    public GameObject ship;
-    public GameObject laserPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -18,21 +16,10 @@ public class LaserBehaviourScript : MonoBehaviour
     {
         //Este script estaria atado al boton de disparar y al laser, confirmamos que el gameobject
         //sea el laser y lo movemos continuamente hasta que alcanze una posicion en pantalla para destruirlo
-        if(gameObject.name == "soap laser(Clone)"){
-            transform.Translate(Vector3.left * 10 * Time.deltaTime);
-            if(gameObject.GetComponent<Transform>().position.x < -14){
-                Debug.Log(gameObject.name);
-                Destroy(gameObject);
-            }
-        }   
-    }
-
-    //TODO: hacer que no aparezcan tantos jabones juntos
-    void OnMouseDown(){
-        if(gameObject.name == "shoot-button"){
-            Vector3 shipPosition = ship.GetComponent<Transform>().position;
-            Debug.Log(shipPosition);
-            Instantiate(laserPrefab, shipPosition, Quaternion.identity);
-        } 
+        transform.Translate(Vector3.left * 10 * Time.deltaTime);
+        if(gameObject.GetComponent<Transform>().position.x < -14){
+            Debug.Log(gameObject.name);
+            Destroy(gameObject);
+        }  
     }
 }
