0008e3202df441d457b980c0081bb504472dad6f
marco <20160370@ce.pucmm.edu.do>
2021-03-20 22:56:59 -0400

    Game Over when reaching 500 points

--- Assets/Scripts/SpaceshipBehaviourScript.cs
+++ Assets/Scripts/SpaceshipBehaviourScript.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SpaceshipBehaviourScript : MonoBehaviour
 {
     public GameObject soapLaser;
     public GameObject balloonSpawner;
     public GameObject gameOverCanvas;
+    public TMP_Text gameOverText;
+    public TMP_Text gameOverButton;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +30,10 @@ public class SpaceshipBehaviourScript : MonoBehaviour
         {
             Destroy(gameObject);
             Destroy(balloonSpawner.GetComponent<BalloonSpawnerBehaviourScript>());
+
+            //Cambiando el texto del canvas
+            gameOverText.text = "GAME OVER";
+            gameOverButton.text = "RETRY";
             gameOverCanvas.SetActive(true);
         }
     }
